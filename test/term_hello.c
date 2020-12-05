0b05a5ba8dcb509727f38c56c138c427d12715e7
xdrie <xdrie@alt.icu>
2020-12-04 17:55:00 -0800

    use string literal

--- test/term_hello.c
+++ test/term_hello.c
@@ -2,17 +2,7 @@
 #define TERM_MAP_ADDR (char *)0x7000
 #define TERM_DEV_ID 1
 
-char msg[MSG_SZ];
-
-void load_data() {
-    msg[0] = 'h';  // h
-    msg[1] = 'e';  // e
-    msg[2] = 'l';  // l
-    msg[3] = 'l';  // l
-    msg[4] = 'o';  // o
-    msg[5] = '\n'; // \n
-    msg[6] = 0;
-}
+char msg[MSG_SZ] = "hello\n";
 
 __regsused("r0/r1/r2") void term_init(__reg("r0") int dev_id, __reg("r2") volatile char *map_addr) {
     // map the terminal and return the address
@@ -39,7 +29,6 @@ __regsused("r0/r1") void term_flush(__reg("r0") int dev_id) {
 }
 
 int main() {
-    load_data();
     volatile char *h_term = TERM_MAP_ADDR;
     int device_id = TERM_DEV_ID; // terminal device
     term_init(device_id, h_term);
