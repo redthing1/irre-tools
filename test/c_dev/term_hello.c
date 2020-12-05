151fdb8f244baee9b0157444b94e11fdd533f01c
xdrie <xdrie@alt.icu>
2020-12-05 13:39:57 -0800

    move functions to header

--- test/c_dev/term_hello.c
+++ test/c_dev/term_hello.c
@@ -1,3 +1,5 @@
+#include "corlib.h"
+
 #define MSG_SZ 16 // the size of our message string
 #define TERM_MAP_ADDR (char *)0x7000 // the location (changeable) where we map the terminal
 #define TERM_DEV_ID 1 // device ID of the terminal
@@ -5,17 +7,6 @@
 #define TERM_CMD_FLUSH 0x10 // the command ID for "flush"
 
 char msg[MSG_SZ] = "hello, world!\n";
-
-/* inline assembly function to call the SND instruction for I/O */
-__regsused("r0/r1/r2") void __dev_msg(__reg("r0") int dev_id, __reg("r1") int cmd,
-                                    __reg("r2") int arg) = "\tsnd r2 r0 r1"; // devices[dev_id].send(cmd, arg)
-
-/* copy data between two memory locations */
-void __memcpy(volatile char *dst, volatile char *src, int count) {
-    for (int i = 0; i < count; i++) {
-        dst[i] = src[i];
-    }
-}
 
 /* map the terminal device buffer to an address */
 void term_init(int dev_id, volatile char *map_addr) {
