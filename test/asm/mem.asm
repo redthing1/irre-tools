7fb30181a31e03b2ab57979a684eccbac193e0d1
redthing1 <redthing1@alt.icu>
2022-04-02 12:10:42 -0700

    fix mem asm test

--- test/asm/mem.asm
+++ test/asm/mem.asm
@@ -3,14 +3,14 @@
 %entry :main
 
 test1:
-    int r3
+    mov r0 r3 ; set return value
     hlt
 
 main:
     set r1 $f00
     set r2 $41 ; 'a'
     
-    stw r1 r2 #0 ; store r2 to mem
+    stw r2 r1 #0 ; store r2 to mem
     ldw r3 r1 #0 ; load r3 from mem
 
     jmi ::test1
