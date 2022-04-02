65540e3258de8f35bc98ede56df252bd67e90bc5
redthing1 <redthing1@alt.icu>
2022-04-02 01:54:23 -0700

    neater device logging

--- src/irre/source/irre/emulator/devices/ping.d
+++ src/irre/source/irre/emulator/devices/ping.d
@@ -3,6 +3,7 @@ module irre.emulator.devices.ping;
 import irre.emulator.device;
 import std.string;
 import std.stdio;
+import irre.util;
 
 class PingDevice : Device {
     enum Command : WORD {
@@ -10,7 +11,7 @@ class PingDevice : Device {
     }
 
     override WORD recieve(WORD command, WORD data) {
-        writefln("[PING] recieved (command: %d, data: %d)", command, data);
+        log_put(format("[PING] recieved (command: %d, data: %d)", command, data));
         return 0; // success
     }
 }
