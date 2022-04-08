409efdfda6c1c73d55fb6699177bd7b73c21e590
redthing1 <redthing1@alt.icu>
2022-04-07 22:11:24 -0700

    better error message for unprintable infotype

--- src/irre/source/irre/analysis/commit.d
+++ src/irre/source/irre/analysis/commit.d
@@ -95,7 +95,7 @@ template InfoLog(TRegWord, TMemWord, TRegSet, int register_count) {
                 case InfoType.Device:
                     sb ~= format("dev#%02x(%02x)", data, value);
                     break;
-                default: assert(0);
+                default: assert(0, format("unhandled info node to string for type %s", type));
             }
 
             return sb.array;
