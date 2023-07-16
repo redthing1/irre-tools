02e246885699d3df08236141f112c533aa274c2b
redthing1 <redthing1@alt.icu>
2023-07-16 11:38:45 -0700

    add prime number counter

---
+++ test/c_basic/prime_count.c
@@ -0,0 +1,28 @@
+// prime number counter
+
+int is_prime(int m) {
+    if (m <= 1) {
+        return 0;
+    }
+    for (int i = 2; (i * i) <= m; i++) {
+        if (m % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int result = 0;
+
+    int n = 100;
+    
+    // find all prime numbers <= n
+    for (int i = 2; i <= n; i++) {
+        if (is_prime(i)) {
+            result++;
+        }
+    }
+
+    return result;
+}
