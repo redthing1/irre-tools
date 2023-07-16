3d65cf487fbb2deb70812446d8d29a8bc06d4778
redthing1 <redthing1@alt.icu>
2023-07-16 11:38:11 -0700

    add logic branching macros

--- src/irre/assembler/builtins.d
+++ src/irre/assembler/builtins.d
@@ -7,6 +7,13 @@ class BuiltinMacros {
     public MacroDef MACRO_ADI;
     public MacroDef MACRO_SBI;
     public MacroDef MACRO_YEET;
+    public MacroDef MACRO_CMP;
+    public MacroDef MACRO_BEQ;
+    public MacroDef MACRO_BNE;
+    public MacroDef MACRO_BLT;
+    public MacroDef MACRO_BGE;
+    public MacroDef MACRO_BGT;
+    public MacroDef MACRO_BLE;
 
     this() {
         MACRO_ADI = MacroDef("adi", [
@@ -34,5 +41,64 @@ class BuiltinMacros {
                     ]),
                 ]);
         MACRO_YEET = MacroDef("yeet", [], [SourceStatement("nop")]);
+
+        // - conditional stuff
+        MACRO_CMP = MacroDef("cmp", [
+                MacroArg(MacroArg.Type.REGISTER, "rA"),
+                MacroArg(MacroArg.Type.REGISTER, "rB"),
+                ], [
+                SourceStatement("tcu", [Token("ad", CharType.IDENTIFIER)],
+                    [Token("rA", CharType.IDENTIFIER)], [
+                        Token("rB", CharType.IDENTIFIER)
+                    ]),
+                ]);
+        MACRO_BEQ = MacroDef("beq", [MacroArg(MacroArg.Type.VALUE, "v0")],
+                [
+                    SourceStatement("set", [Token("at", CharType.IDENTIFIER)],
+                        [Token("v0", CharType.IDENTIFIER)]),
+                    SourceStatement("bve", [Token("at", CharType.IDENTIFIER)],
+                        [Token("ad", CharType.IDENTIFIER)], 
+                        [Token("#0", CharType.NUMERIC_CONSTANT)]),
+                ]);
+        MACRO_BNE = MacroDef("bne", [MacroArg(MacroArg.Type.VALUE, "v0")],
+                [
+                    SourceStatement("set", [Token("at", CharType.IDENTIFIER)],
+                        [Token("v0", CharType.IDENTIFIER)]),
+                    SourceStatement("bvn", [Token("at", CharType.IDENTIFIER)],
+                        [Token("ad", CharType.IDENTIFIER)], 
+                        [Token("#0", CharType.NUMERIC_CONSTANT)]),
+                ]);
+        MACRO_BLT = MacroDef("blt", [MacroArg(MacroArg.Type.VALUE, "v0")],
+                [
+                    SourceStatement("set", [Token("at", CharType.IDENTIFIER)],
+                        [Token("v0", CharType.IDENTIFIER)]),
+                    SourceStatement("bve", [Token("at", CharType.IDENTIFIER)],
+                        [Token("ad", CharType.IDENTIFIER)], 
+                        [Token("#-1", CharType.NUMERIC_CONSTANT)]),
+                ]);
+        MACRO_BGE = MacroDef("bge", [MacroArg(MacroArg.Type.VALUE, "v0")],
+                [
+                    SourceStatement("set", [Token("at", CharType.IDENTIFIER)],
+                        [Token("v0", CharType.IDENTIFIER)]),
+                    SourceStatement("bvn", [Token("at", CharType.IDENTIFIER)],
+                        [Token("ad", CharType.IDENTIFIER)], 
+                        [Token("#-1", CharType.NUMERIC_CONSTANT)]),
+                ]);
+        MACRO_BGT = MacroDef("bgt", [MacroArg(MacroArg.Type.VALUE, "v0")],
+                [
+                    SourceStatement("set", [Token("at", CharType.IDENTIFIER)],
+                        [Token("v0", CharType.IDENTIFIER)]),
+                    SourceStatement("bve", [Token("at", CharType.IDENTIFIER)],
+                        [Token("ad", CharType.IDENTIFIER)], 
+                        [Token("#1", CharType.NUMERIC_CONSTANT)]),
+                ]);
+        MACRO_BLE = MacroDef("ble", [MacroArg(MacroArg.Type.VALUE, "v0")],
+                [
+                    SourceStatement("set", [Token("at", CharType.IDENTIFIER)],
+                        [Token("v0", CharType.IDENTIFIER)]),
+                    SourceStatement("bvn", [Token("at", CharType.IDENTIFIER)],
+                        [Token("ad", CharType.IDENTIFIER)], 
+                        [Token("#1", CharType.NUMERIC_CONSTANT)]),
+                ]);
     }
 }
