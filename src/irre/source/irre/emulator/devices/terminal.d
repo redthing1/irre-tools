970b7bbd91f62ed598e936395794536301f821b4
redthing1 <redthing1@alt.icu>
2023-07-16 11:38:46 -0700

    better IO test with readf readln readchar etc

--- src/irre/source/irre/emulator/devices/terminal.d
+++ src/irre/source/irre/emulator/devices/terminal.d
@@ -3,15 +3,19 @@ module irre.emulator.devices.terminal;
 import irre.emulator.device;
 import std.string;
 import std.stdio;
+import std.conv;
 import irre.util;
 
 class TerminalDevice : MappedDevice {
     enum Command : WORD {
         FLUSH = 0x10,
-        READKEY = 0x11,
-        // SETCURSOR = 0x12,
-        // GETCURSOR = 0x13,
-        // SETATTR = 0x14,
+        READCHAR = 0x11,
+        WRITECHAR = 0x12,
+        READLN = 0x13,
+        READF = 0x14,
+        // SETCURSOR = 0x16,
+        // GETCURSOR = 0x17,
+        // SETATTR = 0x20,
     }
 
     this() {
@@ -24,7 +28,7 @@ class TerminalDevice : MappedDevice {
     }
 
     override WORD recieve(WORD command, WORD data) {
-        log_put(format("[TERM] recieved (comamnd: %08x, data: %d)", command, data));
+        log_put(format("[TERM] recieved (command: $%04x, data: %d)", command, data));
         WORD result = super.recieve(command, data);
         if (result == 0)
             return result;
@@ -42,13 +46,37 @@ class TerminalDevice : MappedDevice {
                     write(ch);
                 }
                 // clear the memory block
-                // TODO
+                for (int i = 0; i < mapped_block_size; i++)
+                    buffer[i] = 0;
+                vm.write_bytes(map_address, buffer, mapped_block_size);
                 return 0;
             }
-        case Command.READKEY: {
-                // TODO
+        case Command.READCHAR: {
+                // read a key from the console
+                auto ch = getchar();
+                return ch;
+            }
+        case Command.WRITECHAR: {
+                // write a character to the console
+                auto ch = cast(char) data;
+                putchar(ch);
                 return 0;
             }
+        case Command.READLN: {
+            auto read_str = stdin.readln();
+            auto read_data =  cast(ubyte[]) read_str.to!(char[]);
+            vm.write_bytes(map_address, read_data, read_data.length);
+
+            return cast(UWORD) read_data.length;
+        }
+        case Command.READF: {
+            // read data from stdin
+            auto buffer = new ubyte[mapped_block_size];
+            auto read_data = stdin.rawRead(buffer);
+            vm.write_bytes(map_address, read_data, read_data.length);
+
+            return cast(UWORD) read_data.length;
+        }
         default:
             return -1; // unhandled
         }
