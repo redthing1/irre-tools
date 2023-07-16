e5fa4b493711bd795f53a7e31e306499de4c2cee
redthing1 <redthing1@alt.icu>
2023-07-16 11:38:10 -0700

    add broken hello

--- test/string.c
+++ test/string.c
@@ -1,6 +1,18 @@
-char *msg = "hello";
+int letters[10] = {};
+
+void load_letters() {
+    letters[0] = 'h';  // h
+    letters[1] = 'e';  // e
+    letters[2] = 'l';  // l
+    letters[3] = 'l';  // l
+    letters[4] = 'o';  // o
+    letters[5] = '\n'; // \n
+    letters[6] = 0;
+}
 
 int main()
 {
-    return msg[0];
+    load_letters();
+
+    return letters[0];
 }
