8a3a9bb097b0830e240f0dc72bc4d199e2f98208
redthing1 <redthing1@alt.icu>
2023-07-16 11:38:47 -0700

    add a simple program to check

---
+++ test/c_dbg/test1.c
@@ -0,0 +1,33 @@
+#include "../lib/corlib.h"
+
+#define BLOCK_SIZE (4)
+
+char arr1[BLOCK_SIZE] = {1, 2, 3, 4};
+char arr2[BLOCK_SIZE] = {9, 8, 7, 6};
+char add_buf[BLOCK_SIZE];
+char arr3[BLOCK_SIZE] = {10, 10, 10, 10}; // correct result
+
+void add_blocks(char *block1, char *block2, char *result) {
+    for (int i = 0; i < BLOCK_SIZE; i++) {
+        result[i] = block1[i] + block2[i];
+    }
+}
+
+bool compare_blocks(char *block1, char *block2) {
+    for (int i = 0; i < BLOCK_SIZE; i++) {
+        if (block1[i] != block2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    add_blocks(arr1, arr2, add_buf);
+
+    if (!compare_blocks(add_buf, arr3)) {
+        return 1;
+    }
+
+    return 0;
+}
