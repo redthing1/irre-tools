728a3ee11b050e7623b14b2b282441ef8d4f9f44
redthing1 <redthing1@alt.icu>
2023-07-16 11:38:47 -0700

    try to improve this broken demo

--- test/c_dbg/test1.c
+++ test/c_dbg/test1.c
@@ -4,7 +4,7 @@
 
 char arr1[BLOCK_SIZE] = {1, 2, 3, 4};
 char arr2[BLOCK_SIZE] = {9, 8, 7, 6};
-char add_buf[BLOCK_SIZE];
+char add_buf[BLOCK_SIZE] = {0, 0, 0, 0};
 char arr3[BLOCK_SIZE] = {10, 10, 10, 10}; // correct result
 
 void add_blocks(char *block1, char *block2, char *result) {
@@ -21,8 +21,8 @@ void add_blocks(char *block1, char *block2, char *result) {
 //     }
 //     return true;
 // }
-bool compare_first(char *block1, char *block2) {
-    return block1[0] == block2[0];
+bool compare_second(char *block1, char *block2) {
+    return block1[1] == block2[1];
 }
 
 int main() {
@@ -33,7 +33,7 @@ int main() {
     //     return 1;
     // }
 
-    if (!compare_first(add_buf, arr3)) {
+    if (!compare_second(add_buf, arr3)) {
         return 1;
     }
 
