16579df87059a853259b230d845640c727a1cd42
redthing1 <redthing1@alt.icu>
2023-07-16 11:38:47 -0700

    add fib4

---
+++ test/c_basic/fib_4.c
@@ -0,0 +1,27 @@
+// dp fibonacci
+
+#define TABLE_SIZE (64 + 1)
+int table[TABLE_SIZE]; // max dp table size
+
+int fib(int n) {
+    // 1. init dp table
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        table[i] = -1;
+    }
+    table[0] = 0;
+    table[1] = 1;
+
+    // 2. dp to calculate fib(n)
+    for (int i = 2; i <= n; i++) {
+        table[i] = table[i - 1] + table[i - 2];
+    }
+
+    return table[n];
+}
+
+int main() {
+    // return fib(7); // 13
+    // return fib(8); // 21
+    // return fib(12); // 144
+    return fib(200);
+}
