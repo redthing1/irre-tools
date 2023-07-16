7beda99611299a0e1e16a498179cee146a1da824
redthing1 <redthing1@alt.icu>
2023-07-16 11:38:10 -0700

    big JMI for entry

--- src/irre/assembler/parser.d
+++ src/irre/assembler/parser.d
@@ -190,12 +190,13 @@ class Parser {
         if (entry_label) {
             // resolve the label and replace the entry jump
             immutable auto entry_label_def = resolve_label(entry_label);
-            immutable auto entry_addr = entry_label_def.offset - INSTRUCTION_SIZE;
-            immutable ubyte entry_addr_lo = entry_addr & 0xff;
-            immutable ubyte entry_addr_hi = cast(ubyte) (entry_addr >> 8);
-            statements.data[0] = AbstractStatement(OpCode.SET,
-                    cast(ValueArg) ValueImm(Register.PC), cast(ValueArg) ValueImm(entry_addr_lo),
-                    cast(ValueArg) ValueImm(entry_addr_hi));
+            immutable auto entry_addr = entry_label_def.offset;
+            immutable ubyte entry_addr_l8 = cast(ubyte) ((entry_addr >> 0) & 0xff);
+            immutable ubyte entry_addr_m8 = cast(ubyte) ((entry_addr >> 8) & 0xff);
+            immutable ubyte entry_addr_h8 = cast(ubyte) ((entry_addr >> 16) & 0xff);
+            statements.data[0] = AbstractStatement(OpCode.JMI,
+                    cast(ValueArg) ValueImm(entry_addr_l8), cast(ValueArg) ValueImm(entry_addr_m8),
+                    cast(ValueArg) ValueImm(entry_addr_h8));
         }
         // resolve statements, rewriting them
         auto resolved_statements = resolve_statements(statements);
