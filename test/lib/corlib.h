7e780aade9c88f5b949d0c44bb0e5595db0d08a7
redthing1 <redthing1@alt.icu>
2023-07-16 11:38:49 -0700

    add more defs to corlib

--- test/lib/corlib.h
+++ test/lib/corlib.h
@@ -18,6 +18,14 @@
 #define int64_t long
 #define uint64_t unsigned long
 #define size_t uint32_t
+#define i8 int8_t
+#define u8 uint8_t
+#define i16 int16_t
+#define u16 uint16_t
+#define i32 int32_t
+#define u32 uint32_t
+#define i64 int64_t
+#define u64 uint64_t
 
 #define BOOL int
 #define true (1)
