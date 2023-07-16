e4a09e13cd28d75c9aa12ecb776d4880c9e7c925
redthing1 <redthing1@alt.icu>
2023-07-16 11:38:44 -0700

    add device info type

--- src/irre/source/irre/emulator/devices/ping.d
+++ src/irre/source/irre/emulator/devices/ping.d
@@ -8,10 +8,26 @@ import irre.util;
 class PingDevice : Device {
     enum Command : WORD {
         PING = 0x01,
+        COUNT = 0x02,
     }
+
+    private int ping_count = 0;
 
     override WORD recieve(WORD command, WORD data) {
         log_put(format("[PING] recieved (command: %d, data: %d)", command, data));
-        return 0; // success
+
+        switch (command) {
+        case Command.PING: {
+                ping_count++;
+                return 0; // success
+            }
+        case Command.COUNT: {
+                return ping_count;
+            }
+        default:
+            break;
+        }
+
+        return -1; // unhandled
     }
 }
