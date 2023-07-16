5005c498d748976c6672e41717f028de9ae16bcf
redthing1 <redthing1@alt.icu>
2023-07-16 11:38:48 -0700

    update build script and irrecc

--- build.sh
+++ build.sh
@@ -3,12 +3,18 @@
 # 0. preparations
 git submodule update --init --recursive
 
-# 1. build vbcc
+# 1. build compiler
 pushd tools/vbcc
 export VBCC=`pwd`
 # make clean
 mkdir -p bin
 make -j TARGET=irre DTAUTO=1 all bin/vbccirre
+popd
+
+pushd tools/chcc
+export CHCC=`pwd`
+# make clean
+make -j
 popd
 
 # 2. build irre tools
