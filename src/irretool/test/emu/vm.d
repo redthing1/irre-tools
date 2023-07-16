fc9cf6a01e0bdee8d3db4a710606ed348419a959
redthing1 <redthing1@alt.icu>
2023-07-16 11:38:45 -0700

    magic program verification in vm

--- src/irretool/test/emu/vm.d
+++ src/irretool/test/emu/vm.d
@@ -22,42 +22,14 @@ unittest {
 
 @("vm.exec.fib2")
 unittest {
-    auto prg = PROG_FIB2;
-    auto bin = compile_program(prg);
-    auto hyp = create_hypervisor_for(bin);
+    verify_program(PROG_FIB2, 1024, [
+        Register.R0: 0x08,
+    ]);
+}
 
-    hyp.run(1024); // run up to 1024 steps
-
-    // check result
-    struct Mistake {
-        Register reg;
-        UWORD expect;
-        UWORD actual;
-    }
-
-    auto regs = hyp.vm.reg;
-    UWORD[Register] expect = [
-        Register.R0: 0x08
-    ];
-
-    auto sb = appender!string;
-    Mistake[] mistakes;
-
-    for (auto i = 0; i < REGISTER_COUNT; i++) {
-        auto reg_id = i.to!Register;
-        auto reg_value = regs[reg_id];
-
-        if (reg_id !in expect) {
-            continue;
-        }
-
-        auto expect_value = expect[reg_id];
-
-        if (reg_value != expect_value) {
-            mistakes ~= Mistake(reg_id, expect_value, reg_value);
-            sb ~= format(" (reg %s=$%04x (expect $%04x))", reg_id, reg_value, expect_value);
-        }
-    }
-
-    assert(mistakes.length == 0, format("prog %s incorrect:%s", prg.name, sb.array));
+@("vm.exec.fib3")
+unittest {
+    verify_program(PROG_FIB3, 2048, [
+        Register.R0: 144,
+    ]);
 }
