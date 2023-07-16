4da4cc5f64cadf3b022740a593fe7a654f710d6b
redthing1 <redthing1@alt.icu>
2023-07-16 11:38:44 -0700

    simple ift test 4

---
+++ test/asm/ift4.asm
@@ -0,0 +1,18 @@
+; ift test #4
+
+%entry :main
+
+test1:
+    set r1 #1
+    set r2 #2
+    add r3 r1 r2    ; 1 + 2
+    set r4 #3
+    set r5 #4
+    add r6 r4 r5    ; 3 + 4
+
+    add r0 r3 r6    ; (1 + 2) + (3 + 4)
+
+    ret
+
+main:
+    jmi ::test1
