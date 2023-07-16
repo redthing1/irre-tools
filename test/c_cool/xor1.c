e37c92b493ea2846ea88ff372cc31c9d12627c89
redthing1 <redthing1@alt.icu>
2023-07-16 11:38:47 -0700

    write a broken xor program

---
+++ test/c_cool/xor1.c
@@ -0,0 +1,53 @@
+#include "../lib/corlib.h"
+
+#define BLOCK_SIZE (8)
+
+char xor_input[BLOCK_SIZE] = {'i', 'c', 'e', 'c', 'r', 'e', 'a', 'm'}; // 6963 6563 7265 616d
+char xor_key[BLOCK_SIZE] = {'c', 'a', 'n', 'd', 'y', 'b', 'a', 'r'};   // 6361 6e64 7962 6172
+char xor_encrypted[BLOCK_SIZE];                                        // should be 0a02 0b07 0b07 001f
+char xor_decrypted[BLOCK_SIZE];                                        // should be 6963 6563 7265 616d
+char xor_check[BLOCK_SIZE] = {0x0a, 0x02, 0x0b, 0x07, 0x0b, 0x07, 0x00, 0x1f};
+
+void xor_blocks(char *block1, char *block2, char *result) {
+    for (int i = 0; i < BLOCK_SIZE; i++) {
+        // result[i] = block1[i] ^ block2[i];
+        __debugger_break();
+        char b1c = block1[i];
+        char b2c = block2[i];
+        char res = b1c ^ b2c;
+        result[i] = res;
+    }
+}
+
+bool compare_blocks(char *block1, char *block2) {
+    for (int i = 0; i < BLOCK_SIZE; i++) {
+        if (block1[i] != block2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    xor_blocks(xor_input, xor_key, xor_encrypted);
+
+    // compare xor_encrypted with xor_check
+    bool xor_encrypted_matches_xor_check = compare_blocks(xor_encrypted, xor_check);
+
+    if (!xor_encrypted_matches_xor_check) {
+        __debugger_break();
+        return 1;
+    }
+
+    // now, xor the output with the key again to get the original input
+    xor_blocks(xor_encrypted, xor_key, xor_decrypted);
+
+    // // compare xor_decrypted with xor_input
+    // bool xor_decrypted_matches_xor_input = compare_blocks(xor_decrypted, xor_input);
+    // if (!xor_decrypted_matches_xor_input) {
+    //     __debugger_break();
+    //     return 2;
+    // }
+
+    return 0;
+}
--- test/lib/corlib.h
+++ test/lib/corlib.h
@@ -1,6 +1,7 @@
 #ifndef CORLIB_H
 #define CORLIB_H
 
+/* types */
 #define CHAR (char)
 #define CHARP (char*)
 #define VCHARP (volatile char*)
@@ -8,11 +9,19 @@
 #define UINT (unsigned int)
 #define VOID (void)
 
-/* inline assembly function to call the SND instruction for I/O */
+#define BOOL int
+#define true (1)
+#define false (0)
+#define bool BOOL
+
+/** inline assembly function to call the SND instruction for I/O */
 __regsused("r0/r1/r2") int __dev_msg(__reg("r1") int dev_id, __reg("r2") int cmd,
                                     __reg("r0") int arg) = "\tsnd r0 r1 r2"; // devices[dev_id].send(cmd, arg)
 
-/* copy data between two memory locations */
+/** intrinsic to break into the debugger */
+void __debugger_break() = "\tint $a0\t; __debugger_break";
+
+/** copy data between two memory locations */
 void __memcpy(volatile char *dst, volatile char *src, int count) {
     for (int i = 0; i < count; i++) {
         dst[i] = src[i];
@@ -39,10 +48,6 @@ void srand(int s) { seed = s; }
 int rand() {
     seed = (rng_a * seed + rng_c) % rng_m;
     return seed;
-}
-
-void __irre_debuger_interrupt() {
-    __asm("\tint $a0");
 }
 
 #endif /* CORLIB_H */
