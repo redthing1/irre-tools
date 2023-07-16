914d8fcb4ad65e379c33aa6691d27843efcbeee2
redthing1 <redthing1@alt.icu>
2023-07-16 11:38:45 -0700

    the minimizer can now do 2-step freezes

--- src/irre/source/irre/analysis/minimizer.d
+++ src/irre/source/irre/analysis/minimizer.d
@@ -31,6 +31,20 @@ class ProgramMinimizer {
             statement.a1 = statement.a2 = statement.a3 = ValueImm(0);
         }
 
+        AbstractStatement* find_statement_at_pc(ProgramAst prog, UWORD pc) {
+            auto offs = 0;
+            for (auto i = 0; i < prog.statements.length; i++) {
+                // check
+                if (offs == pc) {
+                    return &prog.statements[i];
+                }
+
+                offs += INSTRUCTION_SIZE;
+            }
+
+            return null;
+        }
+
         writefln("minimizer");
 
         // 1. look at total clobber of mem and regs
@@ -43,7 +57,13 @@ class ProgramMinimizer {
             auto freezable = true;
             foreach (source; ift.clobbered_regs_sources[reg_id]) {
                 // check whether this source node is freezable too
-                freezable = freezable && source.is_final;
+                bool is_this_freezable = source.is_final;
+                // another case is if it comes from the initial snapshot
+                if (source.commit_id == -1) {
+                    // it came from the initial snapshot
+                    is_this_freezable = true;
+                }
+                freezable = freezable && is_this_freezable;
             }
 
             // blacklist
@@ -62,6 +82,7 @@ class ProgramMinimizer {
         ProgramAst prog = source_program; // copy source program
         UWORD offset = (cast(UWORD) prog.statements.length) * cast(UWORD) INSTRUCTION_SIZE;
         bool[Register] frozen_registers;
+        bool[UWORD] whitelisted_instructions; // these instructions will never be NOPed
         for (int i = (cast(int) prog.statements.length - 1); i >= 0; i--) {
             offset -= INSTRUCTION_SIZE;
             auto stmt = &prog.statements[i];
@@ -74,6 +95,7 @@ class ProgramMinimizer {
 
             // find the last commit at this pc
 
+            // this is the current commit we are searching from
             auto last_commit_here_ix = ift.find_last_commit_at_pc(offset, ift.last_commit_ix);
 
             if (last_commit_here_ix >= 0) {
@@ -87,25 +109,28 @@ class ProgramMinimizer {
                 if (commit_results.length == 1
                     && commit_results[0].type == InfoType.Register) {
                         // get that single result
-                        auto result0 = commit_results[0];
-
-                        log_put(format("   this commit has a single register result: %s", result0));
+                        auto single_result = commit_results[0];
 
                         // get reg id of that single result
-                        auto result_reg_id = result0.data.to!Register;
+                        auto result_reg_id = single_result.data.to!Register;
 
                         // is this register already frozen?
                         if (frozen_registers.get(result_reg_id, false)) {
                             // this register's final value is frozen
-                            // so we can nop this instruction
-                            nop_statement(stmt);
+                            if(!whitelisted_instructions.get(offset, false)) {
+                                // and it is not whitelisted
+                                // so we can nop this instruction
+                                nop_statement(stmt);
+                            }
                             continue; // all done here
                         }
+
+                        log_put(format("    this commit has a single register result: %s", single_result));
                         
                         // if this register is freezable, then can we replace this step with direct setting?
                         if (freezable_registers.get(result_reg_id, false)) {
                             // yes, we can
-                            log_put(format("   commit result %s is of a freezable register %s", result0, result_reg_id));
+                            log_put(format("   commit result %s is of a freezable register %s", single_result, result_reg_id));
 
                             auto final_reg_value = ift.snap_final.reg[result_reg_id];
                             // check if the value will fit in a single 16-bit SET instruction
@@ -121,6 +146,72 @@ class ProgramMinimizer {
                                 stmt.a3 = ValueImm((final_reg_value & 0xff00) >> 8);
 
                                 log_put(format("   replacing with SET %s, %s, %s", stmt.a1, stmt.a2, stmt.a3));
+                            } else {
+                                // this won't fit in a single SET instruction
+                                // we need a SET, SUP sequence.
+                                // this means we need to find the last two commits that set only this register
+
+                                // currently we have already found one commit that set this register
+                                // so we need to find the previous commit touching this register (if any) that sets it alone
+                                // this is O(n^2)
+
+                                log_put(format("   only 2-instruction freeze is possible, searching for candidate prev"));
+
+                                auto found_prev_singleset = false;
+                                auto singleset_search_last_commit_ix = last_commit_here_ix - 1;
+                                
+                                if (singleset_search_last_commit_ix < 0) continue;
+
+                                while (!found_prev_singleset) {
+                                    // look for the last touching commit
+                                    auto prev_toucher_ix = ift.find_commit_last_touching(single_result, singleset_search_last_commit_ix);
+
+                                    // try to verify
+                                    if (prev_toucher_ix < 0) break;
+
+                                    auto prev_toucher_commit = ift.trace.commits[prev_toucher_ix];
+                                    log_put(format("    checking candidate prev: %s", prev_toucher_commit));
+
+                                    auto prev_toucher_results = prev_toucher_commit.as_nodes;
+                                    if (prev_toucher_results.length == 1) {
+                                        // this is what we wanted
+
+                                        assert(prev_toucher_results[0].type == InfoType.Register);
+                                        assert(prev_toucher_results[0].data.to!Register == result_reg_id);
+
+                                        // we found a second-to-last commit that set this register alone
+                                        log_put(format("    accepted candidate prev: %s", prev_toucher_commit));
+
+                                        // find the statement there
+                                        auto prev_touch_stmt = find_statement_at_pc(prog, prev_toucher_commit.pc);
+                                        assert(prev_touch_stmt != null);
+
+                                        // mark as frozen
+                                        frozen_registers[result_reg_id] = true;
+                                        // whitelist instruction at prev (so it isn't nopped cause its reg is frozen)
+                                        whitelisted_instructions[prev_toucher_commit.pc] = true;
+
+                                        // add the SET/SUP sequence
+                                        auto val_lower = final_reg_value & 0xffff;
+                                        auto val_upper = (final_reg_value & 0xffff0000) >> 16;
+
+                                        prev_touch_stmt.op = OpCode.SET;
+                                        prev_touch_stmt.a1 = ValueImm(result_reg_id);
+                                        prev_touch_stmt.a2 = ValueImm((val_lower & 0x00ff));
+                                        prev_touch_stmt.a3 = ValueImm((val_lower & 0xff00) >> 8);
+
+                                        stmt.op = OpCode.SUP;
+                                        stmt.a1 = ValueImm(result_reg_id);
+                                        stmt.a2 = ValueImm((val_upper & 0x00ff));
+                                        stmt.a3 = ValueImm((val_upper & 0xff00) >> 8);                                        
+
+                                        break;
+                                    } else {
+                                        // we failed. try again but searching before the result we got this time
+                                        singleset_search_last_commit_ix = prev_toucher_ix - 1;
+                                        continue;
+                                    }
+                                }
                             }
                         }
                     }
