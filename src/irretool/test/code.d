1a2df704615c6bdc18ebd9ff621a576be2607be0
redthing1 <redthing1@alt.icu>
2023-07-16 11:38:45 -0700

    read string import tests

--- src/irretool/test/code.d
+++ src/irretool/test/code.d
@@ -1,11 +1,13 @@
 module irretool.test.code;
 
-enum PROG_BASIC_ADD = `
-%entry :main
+// enum PROG_BASIC_ADD = `
+// %entry :main
 
-main:
-    set r1 #10
-    set r2 #-5
-    add r0 r1 r2
-    hlt
-`;
+// main:
+//     set r1 #10
+//     set r2 #-5
+//     add r0 r1 r2
+//     hlt
+// `;
+
+enum PROG_BASIC = import("asm/basic.asm");
