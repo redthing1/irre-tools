bf304df3bced2d5166d91daaf6a184fdc90a9b4a
redthing1 <redthing1@alt.icu>
2023-07-16 11:38:43 -0700

    working dp fibonacci

--- test/c_basic/fib_3.c
+++ test/c_basic/fib_3.c
@@ -1,7 +1,22 @@
+// dp fibonacci
+
+#define TABLE_SIZE (64 + 1)
+int table[TABLE_SIZE]; // max dp table size
 
 int fib(int n) {
-    if (n <= 1) return n;
-    return fib(n - 1) + fib(n - 2);
+    // 1. init dp table
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        table[i] = -1;
+    }
+    table[0] = 0;
+    table[1] = 1;
+
+    // 2. dp to calculate fib(n)
+    for (int i = 2; i <= n; i++) {
+        table[i] = table[i - 1] + table[i - 2];
+    }
+
+    return table[n];
 }
 
 int main() {
