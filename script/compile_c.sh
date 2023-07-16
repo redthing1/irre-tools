6475223758c36a36fc50933be7cb8794cafeae50
redthing1 <redthing1@alt.icu>
2023-07-16 11:38:09 -0700

    update compile script

--- script/compile_c.sh
+++ script/compile_c.sh
@@ -1,10 +1,15 @@
-SRC = $1
+#!/usr/bin/env bash
+
+# exit when any command fails
+set -e
+
+SRC=$1
 
 # run clang
 $LLVM_LEG_PATH/build/bin/clang -cc1 -triple leg-unknown-unknown -S -o $SRC.s $SRC
 
 # run legc
-./build/meson-out/irre-legc $SRC.s $SRC.asm
+meson-out/irre-legc $SRC.s $SRC.asm
 
 # run asm
-./build/meson-out/irre-asm $SRC.asm $SRC.bin
+meson-out/irre-asm $SRC.asm $SRC.bin
